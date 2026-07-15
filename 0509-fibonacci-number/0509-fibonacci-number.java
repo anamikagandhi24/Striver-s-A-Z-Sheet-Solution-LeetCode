f2161de3769e1241a1485c191488a38ef33e13d8
Anamika Gandhi <anamikagandhi024@gmail.com>
2026-07-15 21:42:03 +0530

    Time: 0 ms (100%), Space: 42 MB (52.26%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1)return n;
+        int prev=0;
+        int curr=1;
+        int next=0;
+        for(int i=2;i<=n;i++){
+            next=prev+curr;
+            prev=curr;
+            curr=next;
+        }
+        return next;
+    }
+}
