ec4a05e8c5318fa2598810faae68161d3d2f16ce
Anamika Gandhi <anamikagandhi024@gmail.com>
2026-07-28 14:26:47 +0530

    Time: 0 ms (100%), Space: 48.1 MB (47.48%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return helper(x,N);
+    }
+    double helper(double x, long n){
+        if(n==0)return 1;
+        double half=helper(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }else{
+            return half*half*x;
+        }
+    }
+}
