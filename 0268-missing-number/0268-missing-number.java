a9317dc9c7644124b5f57f02488574fbe748739a
Anamika Gandhi <anamikagandhi024@gmail.com>
2026-08-10 14:59:47 +0530

    Time: 0 ms (100%), Space: 47.5 MB (33.28%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans^=i^nums[i];
+        }
+        ans^=n;
+        return ans;
+
+    }
+}
